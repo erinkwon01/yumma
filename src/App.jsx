401493c7b1f325f1b589e4323512175168d2c5a6
erinkwon01 <erinkwon01@gmail.com>
2023-02-18 11:57:54 -0800

    Convex update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useMutation, useQuery } from "../convex/_generated/react";
+import Recipe from "./components/Recipe";
+import Modal from '@material-ui/core/Modal';
 
 export default function App() {
   const messages = useQuery("listMessages") || [];
@@ -13,8 +15,47 @@ export default function App() {
     setNewMessageText("");
     await sendMessage(newMessageText, name);
   }
+
+  const [isOpen, setIsOpen] = useState(false);
+
+  // const recipes = useQuery("listRecipes"); 
+
+  // const [currentRecipe, setCurrentRecipe] = useState({
+  //   caption: '',
+  //   difficulty: '',
+  //   ingredients: '',
+  //   name: '',
+  //   steps:'',
+  //   time:''
+  // });
+
+  // const makeRecipe = useMutation("sendRecipe"); 
+
+  // function onUploadClick(){
+
+  // }
+
+  // async function handleSubmitRecipe(event){
+  //   event.preventDefault(); 
+  //   setCurrentRecipe({
+  //     caption: "",
+  //     difficulty: "",
+  //     ingredients: "",
+  //     name: "",
+  //     steps:"",
+  //     time:"",
+  //   })
+  //   await makeRecipe(currentRecipe);
+  //   console.log(currentRecipe);
+  // }
+
+  function handleClose(){
+    setIsOpen(!IsOpen);
+  }
+
   return (
     <main>
+      
       <h1>Convex Chat</h1>
       <p className="badge">
         <span>{name}</span>
@@ -36,6 +77,12 @@ export default function App() {
         />
         <input type="submit" value="Send" disabled={!newMessageText} />
       </form>
+      <button onClick={setIsOpen}>Open Modal</button>
+      <Modal 
+      open = {isOpen}
+      >
+          <input></input>
+      </Modal>
     </main>
   );
 }
