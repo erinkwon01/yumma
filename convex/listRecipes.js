43f840b5349e6b40123f4d534e11f849a07676bb
Kaylee Tran <kayleetran@Kaylees-Macbook-Pro.local>
2023-02-18 22:12:05 -0800

    changed convex function to help filter

--- convex/listRecipes.js
+++ convex/listRecipes.js
@@ -1,5 +1,8 @@
 import { query } from "./_generated/server";
 
-export default query(async ({ db }) => {
-  return await db.query("recipes").collect();
+export default query(async ({ db }, cuisine) => {
+  let querySoFar = db.query("recipes");
+  if (cuisine) querySoFar = querySoFar.filter(q => q.eq(cuisine, q.field("type")))
+  const recipes = await querySoFar.collect();
+  return recipes;
 });
